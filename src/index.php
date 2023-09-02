ccfddb6cd9e2f817f03cb9e6003e63b779c15170
Adam McGurk <eldermcgurk@gmail.com>
2023-09-02 14:03:23 -0600

    Implementing the import

--- src/index.php
+++ src/index.php
@@ -1,5 +1,28 @@
 <?php
 
-use DynamicsWebApi\Helper;
+use DynamicsWebApi\Client;
+use Ramsey\Uuid\Uuid;
 
 require_once 'vendor/autoload.php';
+
+$solutionFileInput = getenv('SOLUTION_FILE');
+
+if (str_contains($solutionFileInput, '.zip')) {
+	if (file_exists($solutionFileInput)) {
+		$base64File = base64_encode(file_get_contents($solutionFileInput));
+	} else {
+		throw new Exception('Sorry, the file does not exist. We received ' . $solutionFileInput);
+	}
+} else {
+	$base64File = getenv('SOLUTION_FILE');
+}
+
+$client = Client::createInstance();
+$uuid = Uuid::uuid4();
+$guid = $uuid->toString();
+$client->request('/ImportSolution', 'POST', [
+	'OverwriteUnmanagedCustomizations' => true,
+	'CustomizationFile' => $base64File,
+	'PublishWorkflows' => true,
+	'ImportJobId' => $guid,
+]);
